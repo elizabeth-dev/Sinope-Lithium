3e7bb221dc28ebe23387c568f125f4da8776b97d
Elizabeth Martín Campos <eli@zabeth.es>
2020-04-09 15:38:29 +0200

    Prettier

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
  * @format
  */
 
-import {Navigation} from 'react-native-navigation';
+import { Navigation } from 'react-native-navigation';
 import App from './App';
 
 Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
 
 Navigation.events().registerAppLaunchedListener(() => {
-  Navigation.setRoot({
-    root: {
-      stack: {
-        children: [
-          {
-            component: {
-              name: 'com.myApp.WelcomeScreen',
-            },
-          },
-        ],
-      },
-    },
-  });
+	Navigation.setRoot({
+		root: {
+			stack: {
+				children: [
+					{
+						component: {
+							name: 'com.myApp.WelcomeScreen',
+						},
+					},
+				],
+			},
+		},
+	});
 });
