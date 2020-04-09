6add8faf78e23fdfee28287c80419d1235742624
Elizabeth Martín Campos <eli@zabeth.es>
2020-04-09 15:37:58 +0200

    Custom boilerplate

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				extensions: [
+					'.ios.js',
+					'.android.js',
+					'.js',
+					'.ts',
+					'.tsx',
+					'.json',
+				],
+				alias: {
+					'@shared/*': ['src/shared/*'],
+					'@core/*': ['src/core/*'],
+					'@assets/*': ['assets/*'],
+				},
+			},
+		],
+	],
 };
