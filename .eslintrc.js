a070ad0ec4083f2f8c5b625d73965a1aba5a54b6
Elizabeth Martín Campos <eli@zabeth.es>
2020-06-07 02:03:57 +0200

    --

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,15 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	rules: {
-		curly: 'off',
+		curly: ['warn', 'multi'],
 		'array-bracket-spacing': ['warn', 'always'],
+		'object-curly-spacing': ['warn', 'always'],
+		'prettier/prettier': 0,
+		indent: ['warn', 'tab'],
+		'max-len': ['warn', { code: 120 }],
+		'comma-dangle': ['error', 'always-multiline'],
+		'jsx-quotes': ['error', 'prefer-double'],
+		'quotes': ['error', 'single'],
+		'no-unused-vars': ['error']
 	},
 };
